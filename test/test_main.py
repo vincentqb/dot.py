04d723a232fbf7fdfb5991736ac93746f84a40fb
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-09-30 12:07:13 -0400

    consistent naming in test

--- test/test_main.py
+++ test/test_main.py
@@ -21,40 +21,42 @@ def set_env(**environ):
 @pytest.fixture
 def root():
     with tempfile.TemporaryDirectory() as root:
-        path = Path(str(root))
-        home = path / "home"
+        root = Path(str(root))
+        home = root / "home"
         home.mkdir(parents=True)
 
-        candidate = path / "another" / "bashrc"
+        candidate = root / "another" / "bashrc"
         candidate.parent.mkdir(parents=True)
         with open(candidate, "w") as fp:
             fp.write("set -o vi")
 
-        yield path
+        yield root
 
 
 def test_link_unlink_profile(root):
-    home = str(root / "home")
-    profile = str(root / "another")
+    home = root / "home"
+    profile = root / "another"
 
-    main(command="link", home=home, profiles=[profile], dry_run=False)
-    assert (Path(home) / ".bashrc").exists()
+    main(command="link", home=str(home), profiles=[str(profile)], dry_run=False)
+    assert (home / ".bashrc").exists()
 
     main(command="unlink", home=home, profiles=[profile], dry_run=False)
-    assert not (Path(home) / ".bashrc").exists()
+    assert not (home / ".bashrc").exists()
 
 
 def test_link_system_exit(root):
-    home = str(root / "home")
-    profile = str(root / "not_a_profile")
+    home = root / "home"
+    profile = root / "not_a_profile"
     with pytest.raises(SystemExit):
-        main(command="link", home=home, profiles=[profile], dry_run=False)
+        main(command="link", home=str(home), profiles=[str(profile)], dry_run=False)
     with pytest.raises(SystemExit):
-        main(command="link", home=home, profiles=[profile], dry_run=True)
-    assert not (Path(home) / "not_a_profile").is_dir()
+        main(command="link", home=str(home), profiles=[str(profile)], dry_run=True)
+    assert not (home / "not_a_profile").is_dir()
 
 
 def test_link_template(root):
+
+    home = str(root / "home")
 
     profile = root / "default"
     profile.mkdir(parents=True)
@@ -63,11 +65,8 @@ def test_link_template(root):
     with open(candidate, "w") as fp:
         fp.write("export APP_SECRET_KEY=$APP_SECRET_KEY")
 
-    home_str = str(root / "home")
-    profile_str = str(root / "default")
-
     with set_env(APP_SECRET_KEY="abc123"):
-        main(command="link", home=home_str, profiles=[profile_str], dry_run=False)
+        main(command="link", home=str(home), profiles=[str(profile)], dry_run=False)
 
     with open(profile / "env.rendered", "r") as fp:
         assert fp.read() == "export APP_SECRET_KEY=abc123"
