f8e9f87e1f2e542b42aab552d30e0947496a0b06
Vincent Quenneville-Belair <vincentqb@gmail.com>
2023-10-02 21:03:07 -0400

    merging tests.

--- test/test_main.py
+++ test/test_main.py
@@ -27,12 +27,25 @@ def root(tmp_path):
 
 @pytest.mark.parametrize("command", ["link", "unlink"])
 @pytest.mark.parametrize("dry_run", [False, True])
-def test_system_exit(root, command, dry_run):
+def test_system_exit_not_a_home(root, command, dry_run, caplog):
+    home = root / "not_a_home"
+    profile = root / "not_a_profile"
+    with pytest.raises(SystemExit):
+        main(command=command, home=str(home), profiles=[str(profile)], dry_run=dry_run)
+    assert len(caplog.records) == 2  # TODO may wish to also show profile warnings
+    assert not home.is_dir()
+    assert not profile.is_dir()
+
+
+@pytest.mark.parametrize("command", ["link", "unlink"])
+@pytest.mark.parametrize("dry_run", [False, True])
+def test_system_exit_home(root, command, dry_run, caplog):
     home = root / "home"
     profile = root / "not_a_profile"
     with pytest.raises(SystemExit):
         main(command=command, home=str(home), profiles=[str(profile)], dry_run=dry_run)
-    assert not (home / "not_a_profile").is_dir()
+    assert len(caplog.records) == 2
+    assert not profile.is_dir()
 
 
 def test_link_unlink_profile(root):
@@ -160,23 +173,3 @@ def test_link_template_folder(root):
     main(command="link", home=str(home), profiles=[str(profile)], dry_run=False)
     assert not (profile / "folder.rendered").exists()
     assert (home / ".folder.template").is_symlink()
-
-
-def test_home_error(root, caplog):
-    home = root / "not_a_home"
-    profile1 = root / "default"
-    profile2 = root / "another"
-
-    with pytest.raises(SystemExit):
-        main(command="link", home=str(home), profiles=[str(profile1), str(profile2)], dry_run=False)
-    assert len(caplog.records) == 2
-
-
-def test_profile_error(root, caplog):
-    home = root / "home"
-    profile1 = root / "default"
-    profile2 = root / "another"
-
-    with pytest.raises(SystemExit):
-        main(command="link", home=str(home), profiles=[str(profile1), str(profile2)], dry_run=False)
-    assert len(caplog.records) == 3
