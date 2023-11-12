d0834af9a53396673ada3467532a8081cb49a542
Vincent QB, PhD <vincentqb@users.noreply.github.com>
2023-11-13 00:04:57 +0100

    one file for both cli test

--- test/test_cli_dot.py
+++ test/test_cli_dot.py
@@ -3,14 +3,17 @@ import subprocess
 import pytest
 from conftest import is_not_tool
 
-cli = "./dot.py"
+def skipif(cli):
+    return pytest.param(cli, marks=pytest.mark.skipif(
+        is_not_tool(cli), reason=f"{cli} not available"
+    )
 
-
-@pytest.mark.skipif(is_not_tool(cli), reason=f"{cli} not available")
+                    
+@pytest.mark.parametrize("cli", [skipif("dot.py"), skipif("./dot.py")])
 @pytest.mark.parametrize("command", ["link", "unlink"])
 @pytest.mark.parametrize("home_folder", ["home", "not_a_home"])
 @pytest.mark.parametrize("dry_run", [False, True])
-def test_error_code_cli(root, command, home_folder, dry_run):
+def test_error_code_cli(cli, root, command, home_folder, dry_run):
     home = root / home_folder
     profile = root / "not_a_profile"
 
@@ -29,14 +32,14 @@ def test_error_code_cli(root, command, home_folder, dry_run):
     assert not profile.is_dir()
 
 
-@pytest.mark.skipif(is_not_tool(cli), reason=f"{cli} not available")
+@pytest.mark.parametrize("cli", [skipif("dot.py"), skipif("./dot.py")])
 @pytest.mark.parametrize("command", ["", "link", "unlink"])
 def test_error_code_help_cli(root, command):
     error_code = subprocess.call([cli] + ([command] if command else []) + ["-h"])
     assert error_code == 0
 
 
-@pytest.mark.skipif(is_not_tool(cli), reason=f"{cli} not available")
+pytest.mark.parametrize("cli", [skipif("dot.py"), skipif("./dot.py")])
 @pytest.mark.parametrize("command", ["", "link", "unlink"])
 def test_error_code_missing_cli(root, command):
     error_code = subprocess.call([cli] + ([command] if command else []))
