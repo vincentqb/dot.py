644222de7243599303b625b042915f7b6104bce1
Vincent QB, PhD <vincentqb@users.noreply.github.com>
2023-11-27 20:43:53 -0500

    move as comment

--- _dot.py
+++ _dot.py
@@ -1,8 +1,5 @@
-"""
-Workaround for dot.py cli to import dot package
-"""
-
 import sys
 
+# Workaround for dot.py cli to import dot package
 sys.path.pop(0)
 from dot.__main__ import dot_from_args  # noqa
