830a96019286beef7b73e79a966f3cec527dc16e
Vincent QB <vincentqb@gmail.com>
2023-11-29 14:32:51 -0500

    reduce number of methods changed for color

--- dot/__main__.py
+++ dot/__main__.py
@@ -14,28 +14,18 @@ class ColoredArgumentParser(ArgumentParser):
     def print_usage(self, file=None):
         if file is None:
             file = sys.stdout
-        self._print_message(self.format_usage()[0].upper() + self.format_usage()[1:], file, YELLOW)
+        self._print_message(YELLOW + self.format_usage()[0].upper() + self.format_usage()[1:] + RESET, file)
 
     def print_help(self, file=None):
         if file is None:
             file = sys.stdout
         self._print_message(self.format_help()[0].upper() + self.format_help()[1:], file)
 
-    def _print_message(self, message, file=None, color=None):
-        if message and color is not None:
-            message = color + message.strip() + RESET + "\n"
-        super()._print_message(message, file)
-
-    def exit(self, status=0, message=None):
-        if message:
-            self._print_message(message, sys.stderr, RED)
-        sys.exit(status)
-
     def error(self, message):
         self.print_usage(sys.stderr)
         message = message.strip()
         message = message[0].upper() + message[1:]
-        self.exit(2, f"Error: {self.prog}: {message}\n")
+        self.exit(2, RED + f"Error: {self.prog}: {message}" + RESET + "\n")
 
 
 def parse_args():
