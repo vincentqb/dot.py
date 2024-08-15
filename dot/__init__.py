af14f5291d2ae631ec218be347d18a675d066aee
Vincent Quenneville-Belair <vincentqb@gmail.com>
2024-08-15 05:28:57 -0400

    move dot to _command

--- dot/__init__.py
+++ dot/__init__.py
@@ -1,21 +1,3 @@
+from ._command import dot
+
 __all__ = ["dot"]
-__ALL__ = dir() + __all__
-
-from ._command import run
-from ._logger import get_counting_logger
-
-
-def __dir__():
-    return __ALL__
-
-
-def dot(command, home, profiles, dry_run):
-    logger = get_counting_logger(dry_run)
-    run(command, home, profiles, True, logger)  # Dry run first
-
-    if logger.warning.counter > 0:
-        logger.error("Error: There were conflicts. Exiting without changing dotfiles.")
-        raise SystemExit(1)
-
-    if not dry_run:
-        run(command, home, profiles, dry_run, logger)  # Wet run second
--- dot/_command.py
+++ dot/_command.py
@@ -3,6 +3,7 @@ import re
 from pathlib import Path
 from string import Template
 
+from ._logger import get_counting_logger
 from ._utils import get_env
 
 
@@ -92,6 +93,18 @@ def run(command, home, profiles, dry_run, logger):
             # Run user requested command
             for func in mapping[command]:
                 func(candidate, rendered, dotfile, dry_run, logger)
+
+
+def dot(command, home, profiles, dry_run):
+    logger = get_counting_logger(dry_run)
+    run(command, home, profiles, True, logger)  # Dry run first
+
+    if logger.warning.counter > 0:
+        logger.error("Error: There were conflicts. Exiting without changing dotfiles.")
+        raise SystemExit(1)
+
+    if not dry_run:
+        run(command, home, profiles, dry_run, logger)  # Wet run second
 
 
 mapping = {"link": [render_recurse, render_single, link], "unlink": [unlink]}
