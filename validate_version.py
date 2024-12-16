b59a593fdab7c883eda6eb741e326fe17d9b2a98
Vincent Quenneville-Belair <vincentqb@gmail.com>
2024-12-16 18:08:13 -0500

    get version from pyproject too

--- validate_version.py
+++ validate_version.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 
-from importlib.metadata import version
-from subprocess import run
+import importlib.metadata
+import subprocess
+
+import tomllib
 
 
 def get_version_from_git():
     cmd = "git describe --tags --abbrev=0".split()
-    result = run(cmd, capture_output=True)
+    result = subprocess.run(cmd, capture_output=True)
     if result.returncode != 0:
-        raise RuntimeError("could not get version from git")
+        raise RuntimeError(
+            f"Could not get version from git\nCaptured stdout:\n{result.stdout}\nCaptured stderr:\n{result.stderr}"
+        )
     return result.stdout.decode().rstrip("\n")
 
 
-def get_version_from_md():
-    return "v" + version("dot.py")
+def get_version_from_package():
+    return "v" + importlib.metadata.version("dot.py")
 
 
-vgit = get_version_from_git()
-vmd = get_version_from_md()
-if vgit == vmd:
+def get_version_from_pyproject():
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+        return "v" + data["project"]["version"]
+
+
+versions = [
+    get_version_from_git(),
+    get_version_from_package(),
+    get_version_from_pyproject(),
+]
+print(versions)
+
+if all(v == versions[0] for v in versions):
     exit(0)
 
 exit(1)
